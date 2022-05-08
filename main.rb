ae01ec46c6155139d32aa9edb13982d5e7389af8
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-08 01:37:00 -0700

    added paint method

--- main.rb
+++ main.rb
@@ -7,11 +7,17 @@ class Draw
     @label = label
     @x = x
     @y = y
+  end
 
-  def button(label_text, x, y, foreground_color, is_dark_mode)
-    if is_dark_mode
+  def paint
+    pass
+  end
+
+  def button(foreground_color, dark_mode)
+    if dark_mode
       paint(label_text, x, y, foreground_color - 10, '#111111')
     else
       paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
   end
+end
