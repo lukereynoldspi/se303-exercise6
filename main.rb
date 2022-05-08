8bc52bb1b1f70b2308efbf220837968f46ac4fa3
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-08 01:34:45 -0700

    defined initialization parameters

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
 class Draw
-  def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+  attr_reader :label
+  attr_reader :x
+  attr_reader :y
+
+  def initialize(label, x, y)
+    @label = label
+    @x = x
+    @y = y
+
+  def button(label_text, x, y, foreground_color, is_dark_mode)
     if is_dark_mode
       paint(label_text, x, y, foreground_color - 10, '#111111')
     else
