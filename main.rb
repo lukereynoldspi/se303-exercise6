dce49bb602ef5270ef5ce3ea879fc2369bc5c035
lukereynoldspi <lukereynoldspi@gmail.com>
2022-05-08 01:31:23 -0700

    created draw class

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+class Draw
+  def draw_button(label_text, x, y, foreground_color, is_dark_mode)
+    if is_dark_mode
+      paint(label_text, x, y, foreground_color - 10, '#111111')
+    else
+      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    end
   end
-end
